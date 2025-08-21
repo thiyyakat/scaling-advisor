94c83e28a77349d97d2ae520b1a22bd0df12c5f8
thiyyakat <meghana.thiyyakat@gmail.com>
2025-08-21 11:46:12 +0530

    Change implementation to use int64 instead of resource.Quantity

--- service/scorer/scorer.go
+++ service/scorer/scorer.go
@@ -40,38 +40,36 @@ func (l LeastCost) Compute(args api.NodeScoreArgs) (api.NodeScore, error) {
 	// resource requests.
 	// Resource quantities of different resource types are reduced to a representation in terms of resource units
 	// based on pre-configured weights.
-	var scheduledResources = make(map[v1.ResourceName]resource.Quantity)
+	var scheduledResources = make(map[v1.ResourceName]int64)
 	//add resources required by pods scheduled on scaled candidate node
 	for _, pod := range args.ScaledAssignment.ScheduledPods {
-		for name, quantity := range pod.AggregatedRequests {
-			if value, found := scheduledResources[name]; found {
-				value.Add(quantity)
-				scheduledResources[name] = value
+		for resourceName, request := range pod.AggregatedRequests {
+			if value, found := scheduledResources[resourceName]; found {
+				scheduledResources[resourceName] = value + request
 			} else {
-				scheduledResources[name] = quantity.DeepCopy()
+				scheduledResources[resourceName] = request
 			}
 		}
 	}
 	//add resources required by pods scheduled on existing nodes
 	for _, assignment := range args.Assignments {
 		for _, pod := range assignment.ScheduledPods {
-			for name, quantity := range pod.AggregatedRequests {
-				if value, found := scheduledResources[name]; found {
-					value.Add(quantity)
-					scheduledResources[name] = value
+			for resourceName, request := range pod.AggregatedRequests {
+				if value, found := scheduledResources[resourceName]; found {
+					scheduledResources[resourceName] = value + request
 				} else {
-					scheduledResources[name] = quantity.DeepCopy()
+					scheduledResources[resourceName] = request
 				}
 			}
 		}
 	}
 	//calculate total scheduledResources in terms of resource units using weights
 	var aggregator float64
-	for name, quantity := range scheduledResources {
-		if weight, found := l.weights[name]; !found {
-			return api.NodeScore{}, fmt.Errorf("no weight found for resource %s", name)
+	for resourceName, quantity := range scheduledResources {
+		if weight, found := l.weights[resourceName]; !found {
+			return api.NodeScore{}, fmt.Errorf("no weight found for resource %s", resourceName)
 		} else {
-			aggregator += weight * float64(quantity.Value())
+			aggregator += weight * float64(quantity)
 		}
 	}
 	//divide total scheduledResources by instance price to get score
@@ -101,42 +99,40 @@ func (l LeastWaste) Compute(args api.NodeScoreArgs) (api.NodeScore, error) {
 	//	Relative wastage can be calculated by summing the wastage on the scaled candidate node
 	//	and the "negative" waste created as a result of unscheduled pods being scheduled on to existing nodes.
 	//  Existing nodes include simulated winner nodes from previous runs.
-	var wastage = make(map[v1.ResourceName]resource.Quantity)
+	var wastage = make(map[v1.ResourceName]int64)
 	//start with allocatable of scaled candidate node
-	for name, quantity := range args.ScaledAssignment.Node.Allocatable {
-		wastage[name] = quantity.DeepCopy()
+	for resourceName, quantity := range args.ScaledAssignment.Node.Allocatable {
+		wastage[resourceName] = quantity
 	}
 	//subtract current usage to find wastage on scaled candidate node
 	for _, pod := range args.ScaledAssignment.ScheduledPods {
-		for name, quantity := range pod.AggregatedRequests {
-			if value, found := wastage[name]; found {
-				value.Sub(quantity)
-				wastage[name] = value
+		for resourceName, request := range pod.AggregatedRequests {
+			if waste, found := wastage[resourceName]; found {
+				wastage[resourceName] = waste - request
 			} else {
-				return api.NodeScore{}, fmt.Errorf("scaled node does not support resource %s required by pod %s", name, pod.Name)
+				return api.NodeScore{}, fmt.Errorf("scaled node does not support resourceName %s required by pod %s", resourceName, pod.Name)
 			}
 		}
 	}
 	//subtract resources scheduled on existing nodes to find relative wastage
 	for _, assignment := range args.Assignments {
 		for _, pod := range assignment.ScheduledPods {
-			for name, quantity := range pod.AggregatedRequests {
-				if value, found := wastage[name]; found {
-					value.Sub(quantity)
-					wastage[name] = value
+			for resourceName, request := range pod.AggregatedRequests {
+				if waste, found := wastage[resourceName]; found {
+					wastage[resourceName] = waste - request
 				} else {
-					return api.NodeScore{}, fmt.Errorf("node %s does not support resource %s required by pod %s", assignment.Node.Name, name, pod.Name)
+					return api.NodeScore{}, fmt.Errorf("node %s does not support resourceName %s required by pod %s", assignment.Node.Name, resourceName, pod.Name)
 				}
 			}
 		}
 	}
 	//calculate single score from wastage using weights
 	var aggregator float64
-	for name, quantity := range wastage {
-		if weight, found := l.weights[name]; !found {
-			return api.NodeScore{}, fmt.Errorf("no weight found for resource %s", name)
+	for resourceName, waste := range wastage {
+		if weight, found := l.weights[resourceName]; !found {
+			return api.NodeScore{}, fmt.Errorf("no weight found for resourceName %s", resourceName)
 		} else {
-			aggregator += weight * float64(quantity.Value())
+			aggregator += weight * float64(waste)
 		}
 	}
 	return api.NodeScore{
